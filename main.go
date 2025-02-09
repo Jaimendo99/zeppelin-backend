379701dda8c554d196f4a7a971e2fe8fa6107ff1
Jaimendo99 <jaime.mendoza@udla.edu.ec>
2025-02-09 14:42:40 -0500

    update / endpoint

--- main.go
+++ main.go
@@ -8,8 +8,17 @@ func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		e.StdLogger.Print("Hola my friend")
-		return c.String(200, "Hello, World!")
+		user := User{
+			Name: "Jon",
+			Age:  20,
+		}
+		return c.JSON(200, user)
 	})
 
 	e.Logger.Info(e.Start("0.0.0.0:3000"))
+}
+
+type User struct {
+	Name string
+	Age  int
 }
