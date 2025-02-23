d93fe3b37b966dd7df6ec9dbd0794c2faea5b2e3
Jaimendo99 <jaime.mendoza@udla.edu.ec>
2025-02-23 18:17:22 -0500

    i dont know

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -12,26 +12,25 @@ import (
 
 func init() {
 	if err := config.LoadEnv(); err != nil {
-		log.Fatalf("failed to load env: %v", err)
+		log.Default().Printf("main: failed to load env: %v", err)
 	}
 
 	if err := config.InitDb(config.GetConnectionString()); err != nil {
-		log.Fatalf("error connecting to database: %v", err)
+		log.Fatalf("main: error connecting to database: %v", err)
 	}
 
 	if err := config.InitMQ(config.GetMQConnectionString()); err != nil {
-		log.Fatalf("error connecting to message queue: %v", err)
+		log.Fatalf("main: error connecting to message queue: %v", err)
 	}
 
 	if err := config.InitFCM(config.GetFirebaseConn()); err != nil {
-		log.Fatalf("error connecting to firebase: %v", err)
+		log.Fatalf("main: error connecting to firebase: %v", err)
 	}
 
 	config.InitSmtp(config.GetSmtpPassword())
 	if err := config.CheckSmtpAuth(config.GetSmtpConfig()); err != nil {
-		log.Fatalf("error authenticating smtp: %v", err)
+		log.Fatalf("main: error authenticating smtp: %v", err)
 	}
-
 }
 
 func main() {
@@ -46,5 +45,4 @@ func main() {
 	if err := e.Start("0.0.0.0:3000"); err != nil {
 		e.Logger.Fatal("error starting server: ", err)
 	}
-
 }
