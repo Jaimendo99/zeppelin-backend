0b0c2e2ffe13834b40aed7f7b7d96dce829bc594
Jaimendo99 <jaime.mendoza@udla.edu.ec>
2025-02-23 17:21:42 -0500

    remove loadenv production failed test

--- internal/config/getenv_test.go
+++ internal/config/getenv_test.go
@@ -6,13 +6,6 @@ import (
 	"zeppelin/internal/config"
 )
 
-func TestLoadEnv(t *testing.T) {
-	err := config.LoadEnv()
-	if err == nil {
-		t.Errorf("Expected nil, got %v", err)
-	}
-}
-
 func TestGetConnectionString(t *testing.T) {
 	os.Setenv("CONNECTION_STRING", "test")
 	result := config.GetConnectionString()
