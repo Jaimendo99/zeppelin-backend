1c3538e9b86e58aa24511692be230ad29b7b76ea
Jaimendo99 <jaime.mendoza@udla.edu.ec>
2025-04-15 17:37:01 -0500

    changes the load env function

--- internal/config/loadenv.go
+++ internal/config/loadenv.go
@@ -2,14 +2,19 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() error {
 	err := godotenv.Load(".env")
-	if err != nil {
+	if err != nil && !os.IsNotExist(err) {
 		return fmt.Errorf("error loading .env file: %w", err)
+	}
+	if os.IsNotExist(err) {
+		log.Println("info: .env file not found, relying on environment variables")
 	}
 	return nil
 }
